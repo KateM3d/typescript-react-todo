a4d10f9c9d404b4d29203a1d8a0bb071219675b1
KateM3d <ekaterina8medvedeva@gmail.com>
2022-04-03 13:50:55 -0400

    added state

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./data";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAddButtom;
 
   return (
     <div className="App">
